5c4a84d6fe9e3d6eb73c2f842ac3721e90cfb716
Luiz1n <64563249+luiz1n@users.noreply.github.com>
2021-07-03 17:41:12 -0300

    atualizacao

--- script.php
+++ script.php
@@ -11,7 +11,7 @@ $furnidata_txt_link = "https://swf.habbocity.me/gamedata/furnidata_2884441y95999
 $arrRequestHeaders = array('http' => array('method' =>'GET', 'protocol_version' => 1.1, 'follow_location' => 1, 'header'=> "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"));
 $data = file_get_contents($furnidata_txt_link, false,  stream_context_create($arrRequestHeaders));
 $data = str_replace(["&", "<"], ["et", ""], utf8_encode($data));
-echo "\nHabboCity All Furnis Downloader\nScript by PHB - https://discord.gg/xeK9EBrnaR\n\n\n";
+echo "\nHabboCity All Furnis Downloader\nScript by Luizin - https://discord.gg/kQuM4YPr\n\n\n";
 foreach(explode('[',$data) as $premier) {
     $var = explode('",',$premier);
     $var = str_replace(['"', '],'], ['', ''], $var);
